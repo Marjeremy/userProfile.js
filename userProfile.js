a23564c59fdba8de29358897de05a1d9d35b6d79
Marjeremy <gaoat856@gmail.com>
2024-11-04 18:20:39 +0800

    userProfile.js commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullname = "Mark Jeremy Gaoat";
+let age = 19;
+let favoriteNumver = 24;
+let favoriteColor = "Black";
+
+console.log("Full name:", fullname);
+console.log("Age:", age);
+console.log("Favorite number:", favoriteNumver);
+console.log("Favorite Color:", favoriteColor);
